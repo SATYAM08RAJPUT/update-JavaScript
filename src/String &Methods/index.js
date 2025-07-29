fc54e6c8b8281f18e24179c15263cdc1d81400e9
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-07-29 23:03:58 +0530

    Done one concept

--- src/String &Methods/index.js
+++ src/String &Methods/index.js
@@ -133,3 +133,4 @@ let str16 = "Hello, world!";
 console.log("slice: ", str16.slice(0,5))
 console.log("Slice: ", str16.slice(7,13))
 console.log("slice: ", str16.slice(0))
+console.log("slice:",str16.slice(7))
