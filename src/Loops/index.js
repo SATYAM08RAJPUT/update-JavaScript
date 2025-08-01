8b635c9374876af16603de49231dbfdb7144d20e
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-08-01 21:30:20 +0530

    modified Question.7

--- src/Loops/index.js
+++ src/Loops/index.js
@@ -176,10 +176,10 @@ console.log("Smallest Number:",storeSmallest)
 console.log("Question.7: ")
 //Question.7: Count how many digits are in a number.
 
-let guesNumber = 23458;
+let guesNumber = 8954884738;
 let digitCount = 0
 
 for(let i=guesNumber;i>0;i=(Math.floor(i/10))){
         digitCount++
 }
-console.log(digitCount)
+console.log("Digits: -",digitCount)
