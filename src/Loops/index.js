a027521f59e02372961003eec321e5d863e9f2f7
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-08-04 22:17:18 +0530

    Done for 1st Question

--- src/Loops/index.js
+++ src/Loops/index.js
@@ -2,3 +2,7 @@ console.log("JavaScript- Loop Topic")
 console.log("11-08-2025")
 // Question.1: Print numbers from 1 to 10 using a for loop
 console.log("Question.1:")
+
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
