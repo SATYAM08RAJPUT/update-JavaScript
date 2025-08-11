a90388cc5e3de380b9ffccf3e043a22f43bc4dbd
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-08-11 23:46:22 +0530

    Starting 3nd Questions

--- src/Loops/index.js
+++ src/Loops/index.js
@@ -8,10 +8,13 @@ for(let i=1;i<=10;i++){
 }
 
 console.log('Question.2:')
-// Print even numbers between 1 and 50.
+// Question.2: Print even numbers between 1 and 50.
 
 for(let i=0;i<=50;i++){
     if(i%2==0){
         console.log(i)
     }
 }
+
+console.log("Question.3")
+// Question.3: Print odd numbers between 1 and 50.
