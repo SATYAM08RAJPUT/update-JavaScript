4bb70ac61f76520cf13c3cc2345454a6ff5ae2ad
unknown <satyamrajput081998@gmail.com>
2025-08-17 12:17:32 -0400

    Defination of string

--- src/String &Methods/index.js
+++ src/String &Methods/index.js
@@ -1,1 +1,3 @@
 console.log("Now String")
+//Question:  What is a String in JavaScript?
+// Answer:  A string is a sequence of characters enclosed in single quotes (''), double quotes (""), or backticks (``).
