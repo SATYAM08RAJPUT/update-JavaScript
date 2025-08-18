58639f50910feeaaadbfcb1dd401101687db9de4
unknown <satyamrajput081998@gmail.com>
2025-08-18 01:58:06 -0400

    find a indexOf

--- src/String &Methods/index.js
+++ src/String &Methods/index.js
@@ -1,3 +1,17 @@
-console.log("Now String")
+console.log("Now String Topic")
 //Question:  What is a String in JavaScript?
 // Answer:  A string is a sequence of characters enclosed in single quotes (''), double quotes (""), or backticks (``).
+
+// Commonly Used String Methods
+
+// 1. Searching: 
+
+// indexOf() → Finds the first occurrence
+// lastIndexOf() → Finds the last occurrence
+// search() → Finds index using regex
+// includes() → Checks if string contains substring
+// startsWith() → Checks if string starts with given value
+// endsWith() → Checks if string ends with given value
+
+let message = "Hello, welcome to JavaScript world!";
+console.log(message.indexOf("w"))
