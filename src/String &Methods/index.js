34f7737cdaba689c976a0ff1c0a60ef07ae771f4
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-08-20 22:43:02 +0530

    add on more Quetion startsWith() method

--- src/String &Methods/index.js
+++ src/String &Methods/index.js
@@ -111,3 +111,7 @@ console.log("startwith(): ", str12.startsWith("world"))
 let str13 = "JavaScript is awesome!";
 console.log("startwith Position: ", str13.startsWith("is",11))
 console.log("startwith Position: ", str13.startsWith("awesome",14))
+
+let str14 = "JavaScript is awesome!";
+
+console.log("startwith():", str14.startsWith("java"))
