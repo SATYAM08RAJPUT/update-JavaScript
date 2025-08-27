ec2732e0d1bc4682340f86eb6284254453c746a0
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-08-27 22:45:30 +0530

    What is array

--- src/Array/index.js
+++ src/Array/index.js
@@ -1,1 +1,4 @@
 console.log('Welcome to Array')
+// In JavaScript, an array is a special type of object used to store multiple values in a single variable.
+// Unlike regular objects, arrays are ordered, meaning the values are stored in a specific sequence 
+// and can be accessed using an index (position).
