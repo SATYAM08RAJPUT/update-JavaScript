b64cb5afe0ff7ed23b3518fabbca4268a9a0fc9e
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-08-27 22:46:58 +0530

    first method of Git Push

--- src/Array/index.js
+++ src/Array/index.js
@@ -2,3 +2,7 @@ console.log('Welcome to Array')
 // In JavaScript, an array is a special type of object used to store multiple values in a single variable.
 // Unlike regular objects, arrays are ordered, meaning the values are stored in a specific sequence 
 // and can be accessed using an index (position).
+
+
+// 1. push() : Adds one or more elements to the end of an array and returns the new length of the array.
+
