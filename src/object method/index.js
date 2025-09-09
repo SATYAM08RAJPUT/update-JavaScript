f99d2dc7a3d9735666da4a528feb4c55e4f5eeea
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-09 22:37:02 +0530

    add a a new Link

--- src/object method/index.js
+++ src/object method/index.js
@@ -1,2 +1,5 @@
 // Object: An object in JavaScript is a collection of key-value pairs.Keys are called properties.
 // Values can be anything → string, number, array, function, another object.
+
+// Creating an object
+console.log("Creating an object")
