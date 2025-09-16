f089246e0d78e187ec05e2206f4d58fc8aa13aaf
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-16 22:37:05 +0530

    If this is used inside an object method, it refers to the object that owns the method.

--- src/The this Keyword/index.js
+++ src/The this Keyword/index.js
@@ -2,7 +2,11 @@ console.log("this keyword")
 // this: The this keyword in JavaScript is a special reference that changes meaning depending on how 
 // and where it’s used. It doesn’t refer to the function itself, but to the object that is executing 
 // the current function.
-console.log(this)
 
 // 1. Global Context: In the browser, this in the global scope refers to the window object.
 // In Node.js, it refers to global.
+
+console.log(this)
+
+
+// 2. Inside an Object Method: If this is used inside an object method, it refers to the object that owns the method.
