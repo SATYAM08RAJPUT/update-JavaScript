b1e655b9f4548282b8703ada3c0059656cb68288
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-16 22:34:35 +0530

    defination of this keyword

--- src/The this Keyword/index.js
+++ src/The this Keyword/index.js
@@ -1,2 +1,5 @@
 console.log("this keyword")
+// this: The this keyword in JavaScript is a special reference that changes meaning depending on how 
+// and where it’s used. It doesn’t refer to the function itself, but to the object that is executing 
+// the current function.
 console.log(this)
