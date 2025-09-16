090af362cdc6b98670166d0d3519cd4a6b0e0a22
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-16 22:36:07 +0530

    Global of this Keyword

--- src/The this Keyword/index.js
+++ src/The this Keyword/index.js
@@ -3,3 +3,6 @@ console.log("this keyword")
 // and where it’s used. It doesn’t refer to the function itself, but to the object that is executing 
 // the current function.
 console.log(this)
+
+// 1. Global Context: In the browser, this in the global scope refers to the window object.
+// In Node.js, it refers to global.
