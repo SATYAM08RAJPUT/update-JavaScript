be0c299f0e8a68dccb1b7299664588b05db09092
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-18 21:07:25 +0530

    add a new  file

--- src/Constructor Function/index.js
+++ src/Constructor Function/index.js
@@ -1,1 +1,3 @@
 console.log("Welcome to constructor function")
+// A constructor function in JavaScript is a special type of function used to create and initialize objects.
+// It works like a blueprint for making multiple objects with similar properties and methods.
