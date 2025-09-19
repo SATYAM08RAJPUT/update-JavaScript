13a9d266d2ba8ed76fb1bce8d4ccdc61662c928d
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-19 20:52:15 +0530

    add a closure function example

--- src/closure/index.js
+++ src/closure/index.js
@@ -16,3 +16,16 @@ function outerfunction(){
 
 const closureExample = outerfunction()
 closureExample()
+
+function createCounter(){
+    let count = 0;
+
+    return function(){
+        count++;
+        return count;
+    }
+}
+
+
+const counter1 = createCounter();
+console.log(counter1)
