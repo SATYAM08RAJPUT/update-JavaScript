041b748ba715b4a2427f00a308b9466de3af817f
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-20 20:46:44 +0530

    add a closure function

--- src/closure/index.js
+++ src/closure/index.js
@@ -1,3 +1,6 @@
 console.log('Closure')
 // Closure: A closure is created when a function remembers the variables from its outer scope,
 //  even after that outer function has finished executing.
+
+
+// 🔹 Basic Example
