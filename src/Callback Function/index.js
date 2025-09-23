9f8ddf7ef4b7c1a7105d95613c88a409086fddcc
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-23 19:33:44 +0530

    add a example

--- src/Callback Function/index.js
+++ src/Callback Function/index.js
@@ -1,6 +1,8 @@
-console.log("topic of callback function")
+console.log("Topic of callback function")
 // Callback Function:A callback function is a function that is passed as an argument 
 // to another function and is executed after some operation is completed.
 
 // 👉 In simple words:
 // “You give a function to another function, and that function will call it later.”
+
+// Example 1: Basic Callback
