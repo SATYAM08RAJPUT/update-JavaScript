a9422e10653cb3b2bc9d69ebed8740f964accf1d
SATYAM08RAJPUT <satyamrajput081998@gmail.com>
2025-09-29 22:39:13 +0530

    innerText

--- src/Dom/index.js
+++ src/Dom/index.js
@@ -2,4 +2,5 @@ console.log("WELCOME TO DOM")
 let getID = document.getElementById("app")
 console.log("GetId: ",getID)
 getID.innerHTML = "WELCOME TO DOM";
-getID.innerHTML = "WECOME TO SECOND DOM"
+getID.innerHTML = "WECOME TO SECOND DOM";
+getID.innerText ="WELCOME G"
